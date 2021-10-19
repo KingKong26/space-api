cc26389eb368e8e7f8280803b11912d9a72fe872
Vishnu Mohan <vishnumohan1996nov@gmail.com>
2021-10-19 22:19:56 +0530

    cookies: samesite fixed

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -12,8 +12,8 @@ class AuthController {
         console.log(err.message, "message");
         return res.status(409).json({ message: "This email already exists" });
       }
-      let avatarUrl = `https://avatars.dicebear.com/api/human/${req.body.email}.svg`
-      req.body.avatar = avatarUrl
+      let avatarUrl = `https://avatars.dicebear.com/api/human/${req.body.email}.svg`;
+      req.body.avatar = avatarUrl;
       let user = await userHelpers.registerUser(req.body);
       // call token service for token generation
       const { accessToken, refreshToken } = tokenService.generateTokens({
@@ -40,7 +40,7 @@ class AuthController {
   // login user
   async userLogin(req, res) {
     try {
-      console.log(`req.body`, req.body)
+      console.log(`req.body`, req.body);
       const user = await userHelpers.loginUser(req.body);
       const { accessToken, refreshToken } = tokenService.generateTokens({
         _id: user._id,
@@ -52,55 +52,59 @@ class AuthController {
       res.cookie("refreshToken", refreshToken, {
         maxAge: 1000 * 60 * 60 * 24 * 30,
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
       });
-      res.cookie("userId",user._id,{
+      res.cookie("userId", user._id, {
         maxAge: 1000 * 60 * 60 * 24 * 30,
-      })
+        secure: true,
+        sameSite: "none",
+      });
       res.cookie("accessToken", accessToken, {
         maxAge: 1000 * 60 * 60 * 24 * 30,
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
       });
       // send response
       res.status(200).json("User authentication successful");
     } catch (error) {
-      console.log(`error.message`, error.message)
+      console.log(`error.message`, error.message);
       res.status(401).send(error.message);
     }
   }
 
   // get user data after successful jwt handshake
 
-  async getUserData (req,res){
-    try{
-      const userData = await userHelpers.getUserDetailsHelper(req.user._id)
-      res.status(200).json(userData)
-    }catch(err){
-      console.log("Error:",err)
-      res.status(500).json(err)
-    }
-  }
-
-  async getData (req,res){
-    try{
-      console.log("User: ",req.params.id)
-      const userData = await userHelpers.getUserDetailsHelper(req.params.id)
-      console.log("User data: ",userData)
-      res.status(200).json(userData)
-    }catch(err){
-      console.log("Error:",err)
-      res.status(500).json(err)
-    }
-  }
-  async toggleOnline (req,res){
-    console.log(`req.body`, req.body)
+  async getUserData(req, res) {
     try {
-      await userHelpers.toggleOnlineHelper(req.body.toggle,req.user._id)
-      res.status(200).json("Update success!!")
-
+      const userData = await userHelpers.getUserDetailsHelper(req.user._id);
+      res.status(200).json(userData);
     } catch (err) {
-      console.log("Error:",err)
-      res.status(500).json(err)
+      console.log("Error:", err);
+      res.status(500).json(err);
+    }
+  }
 
+  async getData(req, res) {
+    try {
+      console.log("User: ", req.params.id);
+      const userData = await userHelpers.getUserDetailsHelper(req.params.id);
+      console.log("User data: ", userData);
+      res.status(200).json(userData);
+    } catch (err) {
+      console.log("Error:", err);
+      res.status(500).json(err);
+    }
+  }
+  async toggleOnline(req, res) {
+    console.log(`req.body`, req.body);
+    try {
+      await userHelpers.toggleOnlineHelper(req.body.toggle, req.user._id);
+      res.status(200).json("Update success!!");
+    } catch (err) {
+      console.log("Error:", err);
+      res.status(500).json(err);
     }
   }
   // sample for testing db connection
